b7311de7c13a34a6d4c2c15ecfd9a956779f96cb
dina7d <dinadjaidja6@gmail.com>
2026-09-24 01:09:57 +0100

    Changed API url to localhost for web testing

--- appointment_appfront/lib/services/api_service.dart
+++ appointment_appfront/lib/services/api_service.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 class ApiService {
   // Note: 10.0.2.2 is the special IP Android emulators use to reach your computer's localhost.
   // If you run this as a Windows desktop app instead of Android, change it to 'http://localhost:8080/api'.
-  static const String baseUrl = 'http://10.0.2.2:8080/api';
+  static const String baseUrl = 'http://localhost:8080/api';
 
   Future<List<dynamic>> getClients() async {
     final response = await http.get(Uri.parse('$baseUrl/clients'));
